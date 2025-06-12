5bae6c521f0de72c57c67627e4f12145e82c5ab1
Olai <Olaisenvb@gmail.com>
2025-06-12 16:34:58 +0200

    Fixed the naming

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "This will permanently delete Ax-Shell cache, configuration, and remove its entry from hyprland.conf."
+echo "This will permanently delete YZ-Shell cache, configuration, and remove its entry from hyprland.conf."
 read -p "Are you sure you want to continue? [y/N] " confirm
 
 if [[ "$confirm" != "y" && "$confirm" != "Y" ]]; then
@@ -8,8 +8,8 @@ if [[ "$confirm" != "y" && "$confirm" != "Y" ]]; then
     exit 1
 fi
 
-rm -rf ~/.cache/ax-shell
-rm -rf ~/.config/Ax-Shell
+rm -rf ~/.cache/yz-shell
+rm -rf ~/.config/YZ-Shell
 
 conf_file=~/.config/hypr/hyprland.conf
 tmp_file=$(mktemp)
@@ -17,15 +17,15 @@ tmp_file=$(mktemp)
 awk '
 BEGIN { found_comment=0 }
 {
-    if ($0 ~ /# Ax-Shell/) {
+    if ($0 ~ /# YZ-Shell/) {
         found_comment=1
         next
     }
-    if (found_comment && $0 ~ /source[[:space:]]*=[[:space:]]*~\/\.config\/Ax-Shell\/config\/hypr\/ax-shell\.conf/) {
+    if (found_comment && $0 ~ /source[[:space:]]*=[[:space:]]*~\/\.config\/YZ-Shell\/config\/hypr\/yz-shell\.conf/) {
         found_comment=0
         next
     }
     print
 }' "$conf_file" > "$tmp_file" && mv "$tmp_file" "$conf_file"
 
-echo "Ax-Shell data and config removed successfully."
+echo "YZ-Shell data and config removed successfully."
